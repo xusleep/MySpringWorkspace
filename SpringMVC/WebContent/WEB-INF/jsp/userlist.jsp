43e26052a4e9372f8d2846c1969c9d2bd6bedb4f
zhonxu <xusleep@gmail.com>
2014-07-15 20:41:44 +0800

    2014 - 07 - 15 commit spring mvc integrated with ibatis

--- SpringMVC/WebContent/WEB-INF/jsp/userlist.jsp
+++ SpringMVC/WebContent/WEB-INF/jsp/userlist.jsp
@@ -1,7 +1,24 @@
-<%@ page language="java" pageEncoding="ISO-8859-1"%>
+<%@ page language="java" pageEncoding="UTF-8" contentType="text/html;charset=UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<h2>THis is Spring MVC demo page</h2>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<% response.setCharacterEncoding("UTF-8"); %>
+<h2>This is Spring MVC demo page</h2>
+<form name="form1">
+<p>你的姓名：  
+     <input type="text" name="name"> 
+</p> 
+<p>你的年龄：  
+     <input type="text" name="age"> 
+</p> 
+<p> 
+     <input type="Submit" value="提交"> 
+     <input type="Reset" value="重置"> 
+</p> 
+</form>
+<h2>The users are below</h2>
 <c:forEach items="${users}" var="user">
-	<c:out value="${user.username}"></c:out><br/>
-	<c:out value="${user.age }"></c:out><br/>
+	user id : <c:out value="${user.id}"></c:out><br/>
+	user name : <c:out value="${user.name}"></c:out><br/>
+	user age  : <c:out value="${user.age }"></c:out><br/>
+	<p></p>
 </c:forEach>
--- SpringMVC/src/test/UserController.java
+++ SpringMVC/src/test/UserController.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,20 +10,36 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
+import test.mybatis.User;
+import test.mybatis.UserMapper;
+
 public class UserController extends AbstractController{
 
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		List<User> userList = new ArrayList<User>();
-		User userA = new User();
-		User userB = new User();
-		userA.setUsername("张三");
-		userA.setAge(27);
-		userB.setUsername("李四");
-		userB.setAge(27);
-		userList.add(userA);
-		userList.add(userB);
+		System.out.println("  request.getCharacterEncoding() : " + request.getCharacterEncoding());
+		System.out.println(" response.getCharacterEncoding() : " + response.getCharacterEncoding());
+		UserMapper userMapper =(UserMapper)this.getWebApplicationContext().getBean("userMapper");
+		String userName = request.getParameter("name");
+		String strAge = request.getParameter("age");
+		if(userName != null && strAge != null)
+		{
+			//userName = new String(request.getParameter("name").getBytes("ISO-8859-1"), "UTF-8");
+			strAge = request.getParameter("age");
+			System.out.println("userName : " + userName);
+			System.out.println("     age : " + strAge);
+			User userA = new User();
+			userA.setName(userName);
+			try
+			{
+				userA.setAge(Integer.parseInt(strAge));
+				userMapper.insertUser(userA);
+			}
+			catch(Exception ex)
+			{}
+		}
+		List<User> userList = userMapper.queryAll(100);
 		return new ModelAndView("userlist", "users", userList);
 	}
 	
